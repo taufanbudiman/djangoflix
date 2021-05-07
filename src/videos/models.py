89ab2f13e789fcf1ca1022039e331cf2290deac5
Taufan Budiman <taufanbudiman@yahoo.com>
2021-05-07 11:43:13 +0700

    Videos apps

---
+++ src/videos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here
+class Video(models.Model):
+    """this is a model for videos."""
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    video_id = models.CharField(max_length=255)
+
+    class Meta:
+        """Meta definition for video."""
+
+        verbose_name = 'video'
+        verbose_name_plural = 'videos'
+
+    def __str__(self):
+        """Unicode representation of video."""
+        return self.title
