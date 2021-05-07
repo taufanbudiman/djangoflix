53584c20a289e94ec29968afbd2ad9c9a27b24bf
Taufan Budiman <taufanbudiman@yahoo.com>
2021-05-07 13:51:22 +0700

    migrate and testcase

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here
 class Video(models.Model):
     """this is a model for videos."""
 
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=255)
 
--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Video
+
 
 # Create your tests here.
+class VideoModelTestCase(TestCase):
+    def setUp(self):
+        Video.objects.create(title='This is my title')
+
+    def test_valid_title(self):
+        title = 'This is my title'
+        qs = Video.objects.filter(title=title)
+        self.assertTrue(qs.exists())
+
+    def test_created_count(self):
+        qs = Video.objects.count()
+        self.assertEqual(qs, 1)
